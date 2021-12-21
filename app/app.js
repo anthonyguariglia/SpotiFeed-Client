5bd1ddb72c83683e2a442403463baa4228adeb72
anthonyguariglia <aguariglia197@gmail.com>
2021-12-21 12:01:27 -0500

    Add event listeners, clean up config/store setup

--- app/app.js
+++ app/app.js
@@ -4,6 +4,16 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const spotifyEvents = require('../server/events')
+const authEvents = require('../auth/events')
+
 $(() => {
+  $('.get-data').on('click', spotifyEvents.onGetData)
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('.sign-up-button').on('click', authEvents.onSignUpButton)
+  $('.sign-out-button').on('click', authEvents.onSignOut)
+
+  $('.new-playlist').on('submit', spotifyEvents.onCreatePlaylist)
   // your JS code goes here
 })
--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
   production: '<replace-with-heroku-url>',
-  development: 'http://localhost:4741'
+  development: 'http://localhost:3000'
 }
 
 if (window.location.hostname === 'localhost') {
